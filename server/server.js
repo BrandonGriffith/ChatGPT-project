d41ce29ab98e2e194d3d4124ca481294442a996f
BrandonGriffith <BrandonGriffith92@gmail.com>
2023-02-22 18:46:40 -0500

    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { Configuration, OpenAIAPI } from "openai";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
