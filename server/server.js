316419ce1c6947cefe7fb8c8f55e0e80ea949abe
BrandonGriffith <BrandonGriffith92@gmail.com>
2023-04-02 14:12:34 -0400

    update

--- server/server.js
+++ server/server.js
@@ -3,5 +3,6 @@ import cors from "cors";
 import { Configuration, OpenAIAPI } from "openai";
 import * as dotenv from "dotenv";
 
+
 dotenv.config();
 let openConfig = new Configuration( { apiKey : process.env.OPEN_AI_KEY } );
