25aa5f21bdd9baf4615025932fed4c1a2436e5cf
BrandonGriffith <BrandonGriffith92@gmail.com>
2024-01-04 20:19:33 -0500

    update

--- client/script.js
+++ client/script.js
@@ -15,39 +15,39 @@ function typeText(e, text) {
     let index = 0;
     let interval = setInterval( () => {
         if (index < text.length) {
-            e.innerHTML += text.chartAt(index);
+            e.innerHTML += text.charAt(index);
             index += 1;
         } else {
             clearInterval(interval);
         }
     }, 20 );
-}
+};
 
 function generateId() {
     const timestamp = Date.now();
     const ranNum = Math.random().toString(16);
     return `id-${timestamp}-${ranNum}`;
-}
+};
 
 function chatStripe(isAi, value, id) {
     return (
         `
-        <div class='wrapper ${isAi && 'ai'}>
-            <div class='chat'>
-                <div class='profile'>
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
                     <img
-                        src='${isAi ? bot : user}'
-                        alt='${isAi ? 'bot' : 'user'}'
+                        src="${isAi ? 'bot' : 'user'}"
+                        alt="${isAi ? 'bot' : 'user'}"
                     />
                 </div>
-                <div class='message' id='${id}'>
+                <div class="message" id="${id}">
                     ${value}
                 </div>
             </div>
         </div>
         `
     )
-}
+};
 
 async function handleSubmit(e) {
     e.preventDefault();
@@ -59,7 +59,28 @@ async function handleSubmit(e) {
     chat_container.scrollTop = chat_container.scrollHeight;
     const message = document.getElementById(id);
     loader(message);
+const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        prompt: data.get('prompt'),
+        model: 'gpt-3.5-turbo'
+    })
+})
+clearInterval(loadInterval);
+message.innerHTML = '';
+if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.content.trim();
+    typeText(message, parsedData);
+} else {
+    const err = await response.text();
+    message.innerHTML = 'Something went wrong';
+    alert(err);
 }
+};
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => e.keyCode == 13 && handleSubmit(e));
--- server/server.js
+++ server/server.js
@@ -1,8 +1,57 @@
 import express from "express";
 import cors from "cors";
-import { Configuration, OpenAIAPI } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 import * as dotenv from "dotenv";
 
-
 dotenv.config();
-let openConfig = new Configuration( { apiKey : process.env.OPEN_AI_KEY } );
+const openConfig = new Configuration({ apiKey: process.env.OPEN_AI_KEY });
+const openai = new OpenAIApi(openConfig);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    console.log('running get');
+    res.status(200).send({
+        message: "Hello World"
+    })
+});
+let messages = [{
+    "role": "system",
+    "content": "You are an assistant."
+}];
+app.post('/', async (req, res) => {
+    console.log(messages , 'posting messages');
+    if (messages.length > 5) {
+        console.log("deleting messages older than 5");
+        messages = messages.slice(-5);
+    }
+    try {
+        let prompt = req.body.prompt;
+        if (!prompt) {
+            throw new Error("Prompt is required");
+        }
+        prompt = {
+            "role": "user",
+            "content": `${prompt}`
+        }
+        const response = await openai.createChatCompletion({
+            "model": "gpt-3.5-turbo-1106",
+        "messages": [
+            ...messages,
+            prompt
+        ],
+        });
+        messages.push(response.data.choices[0].message);
+        console.log(response.data.choices[0].message, 'new message');
+        res.status(200).send({
+            bot: response.data.choices[0].message,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message || "Something went wrong");
+    }
+});
+
+app.listen(5000, () => console.log("Server is running on port http://localhost:5000"));
