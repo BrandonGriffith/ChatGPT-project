742ee31d73af7f8d2612eee37a9398597461b205
BrandonGriffith <BrandonGriffith92@gmail.com>
2024-01-05 13:43:38 -0500

    update

--- client/script.js
+++ client/script.js
@@ -62,7 +62,7 @@ const handleSubmit = async (e) => {
     const message = document.getElementById(id);
     loader(message);
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch(import.meta.env.VITE_SERVERURL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
